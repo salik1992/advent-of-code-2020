14e68def3ef2c2390c5a679db3fb4869c1a8359e
Vit Salomon <vit.salomon@24i.com>
2020-12-13 11:53:13 +0100

    refactor(day-13): reduced runtime from hours to defacto instant

--- day-13/index.js
+++ day-13/index.js
@@ -1,7 +1,4 @@
 const input = require('./input');
-// const input = (
-// `939
-// 7,13,x,x,59,x,31,19`);
 
 const { departureTimestamp, buses } = (() => {
     const [departureLine, busesLine] = input.split('\n');
@@ -10,11 +7,11 @@ const { departureTimestamp, buses } = (() => {
         buses: busesLine.split(',').map((id, index) => ({
             id: id === 'x' ? id : parseInt(id, 10),
             index,
-        })),
+        })).filter(({ id }) => id !== 'x'),
     };
 })();
 
-const busesWithWaitingTimes = buses.filter((line) => line !== 'x').map(({ id }) => {
+const busesWithWaitingTimes = buses.map(({ id }) => {
     const cycle = Math.ceil(departureTimestamp / id);
     const waitingTime = cycle * id - departureTimestamp;
     return { waitingTime, id };
@@ -27,15 +24,52 @@ const nextBusToTheAirport = busesWithWaitingTimes
 console.log(nextBusToTheAirport.waitingTime * nextBusToTheAirport.id);
 
 // part 2
-const maxId = Math.max(...buses);
-const maxIdIndex = buses.findIndex((id) => id === maxId);
 
-let timestamp = 0;
-while (
-    !buses.every(({ id, index }) => (timestamp - (maxIdIndex - index)) % id === 0)
-) {
-    timestamp += maxId;
-}
+/**
+ * This didn't finish before I:
+ * - made a coffee
+ * - read local news
+ * - drank that coffee
+ * - did dishes
+ * - did laundry
+ * - went out with waste
+ * - thought of a refactor
+ * - wrote the refactored code
+ * - got the result
+ * - wrote this comment
+ * 
+ * const maxId = Math.max(...buses.map(({ id }) => id));
+ * const maxIdIndex = buses.find(({ id }) => id === maxId).index;
+ *
+ * let timestamp = 0;
 
-console.log(timestamp - maxIdIndex);
+ * while (
+ *     !buses.every(({ id, index }) => (timestamp - (maxIdIndex - index)) % id === 0)
+ * ) {
+ *     timestamp += maxId;
+ * }
+ *
+ * console.log(timestamp - maxIdIndex);
+ */
 
+console.log(
+    buses.reduce(({ offset, frequency }, { id: busFrequency, index: busOffset }, arrayIndex) => {
+        let timestamp = offset;
+        let firstOccurence = null;
+        let secondOccurence = null;
+        while ((timestamp + busOffset) % busFrequency !== 0) {
+            timestamp += frequency;
+        }
+        firstOccurence = timestamp;
+        if (arrayIndex === buses.length - 1) return firstOccurence;
+        timestamp += frequency;
+        while ((timestamp + busOffset) % busFrequency !== 0) {
+            timestamp += frequency;
+        }
+        secondOccurence = timestamp;
+        return {
+            offset: firstOccurence,
+            frequency: secondOccurence - firstOccurence,
+        };
+    }, { offset: 1, frequency: 1 }),
+);
